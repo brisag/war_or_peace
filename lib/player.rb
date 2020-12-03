3730052ce3f22931dde17c3f7a0ac0310e090b07
Brisa Garcia <brisagarciaglz@gmail.com>
2020-12-03 15:32:51 -0700

    Add players

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,12 @@
 class Player
-attr_reader :name, :deck
-
+  attr_reader :name, :deck
   def initialize(name, deck)
     @name = name
     @deck = deck
   end
 
-  def has_lost?
-    if @deck.cards.count > 0
-      false
-    else
-      true
-    end
+  def if_player_has_lost
+    true if @deck.cards.empty?
   end
+
 end
